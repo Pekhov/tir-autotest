08fe63ea1908a20f49ebf2ac97031d520c8428fe
Pekhov <alexey@pekhov.com>
2016-12-16 15:08:28 +0300

    Скрипт для ответов в MQ

---
+++ Retail_Svyaz/auto_answer_activemq.rb
@@ -0,0 +1,42 @@
+#coding: utf-8
+require 'stomp'
+require 'rexml/document'
+include REXML
+
+answer_BS_R_STM_ABS_A = '\\\\vm-corint\TIR\ETALON_XML\TIR21-СвязьБанк_Retail\BS_R_STM\BS_R_STM_ABS_A.xml'
+answer_BS_R_CARDACC_ABS_A = '\\\\vm-corint\TIR\ETALON_XML\TIR21-СвязьБанк_Retail\BS_R_CARDACC\BS_R_CARDACC_ABS_A.xml'
+answer_BS_R_CARDSTM_ABS_A = '\\\\vm-corint\TIR\ETALON_XML\TIR21-СвязьБанк_Retail\BS_R_CARDSTM\BS_R_CARDSTM_ABS_A.xml'
+answer_BS_R_CCONV_ABS_A = '\\\\vm-corint\TIR\ETALON_XML\TIR21-СвязьБанк_Retail\BS_R_CCONV\BS_R_CCONV_ABS_A.xml'
+answer_BS_R_CREDITPAY_ABS_A = '\\\\vm-corint\TIR\ETALON_XML\TIR21-СвязьБанк_Retail\BS_R_CREDITPAY\BS_R_CREDITPAY_ABS_A.xml'
+answer_DepositClose_ABS_A = '\\\\vm-corint\TIR\ETALON_XML\TIR21-СвязьБанк_Retail\DepositClose\DepositClose_ABS_A.xml'
+
+loop do
+#Подключаемся к MQ
+client = Stomp::Client.new('admin', 'admin', 'vm-corint2', 61613)
+
+
+request = ''
+client.subscribe('/queue/szvABS_in'){|msg| request << msg.body.to_s}
+client.join(1)
+if request.length > 0
+  request_from_abs = Document.new(request)
+  if request_from_abs.elements['//ns0:BSMessage'].attributes['xmlns:ns0'] == 'BS_R_STM'
+    answer = Document.new(File.open(answer_BS_R_STM_ABS_A){ |file| file.read })
+  elsif request_from_abs.elements['//ns0:BSMessage'].attributes['xmlns:ns0'] == 'BS_R_CARDACC'
+    answer = Document.new(File.open(answer_BS_R_CARDACC_ABS_A){ |file| file.read })
+  elsif request_from_abs.elements['//ns0:BSMessage'].attributes['xmlns:ns0'] == '?'
+    answer = Document.new(File.open(answer_BS_R_CARDSTM_ABS_A){ |file| file.read })
+  elsif request_from_abs.elements['//ns0:BSMessage'].attributes['xmlns:ns0'] == 'BS_R_CCONV'
+    answer = Document.new(File.open(answer_BS_R_CCONV_ABS_A){ |file| file.read })
+  elsif request_from_abs.elements['//ns0:BSMessage'].attributes['xmlns:ns0'] == 'BS_R_CREDITPAY'
+    answer = Document.new(File.open(answer_BS_R_CREDITPAY_ABS_A){ |file| file.read })
+  elsif request_from_abs.elements['//ns0:BSMessage'].attributes['xmlns:ns0'] == '?'
+    answer = Document.new(File.open(answer_DepositClose_ABS_A){ |file| file.read })
+  end
+  answer.elements['//ns1:BSMessage'].attributes['ID'] = request_from_abs.elements['//ns0:BSMessage'].attributes['ID']
+  client.publish('/queue/szvABS_out', answer.to_s) #Кидаем запрос в очередь
+  puts "Обработали запрос с ID= #{request_from_abs.elements['//ns0:BSMessage'].attributes['ID']}"
+end
+client.close
+  sleep 5
+end
